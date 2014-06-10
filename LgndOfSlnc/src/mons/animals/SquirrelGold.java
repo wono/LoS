7a21731a96cf8805fd5385eefddffcb63df8707b
wono <wono@live.com>
2014-06-10 12:40:54 -0700

    lowercased directories

---
+++ LgndOfSlnc/src/mons/animals/SquirrelGold.java
@@ -0,0 +1,9 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *        
+ *  JAVA : SQUIRRELGOLD
+ *                                                                  *
+ *      last modified:  2014/06/09                                  *
+ *      first wrote:    2014/06/09                                  *
+ *                                                                  *
+ *      wono (wonho lim: wono@live.com)                             *
+ *                                                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/

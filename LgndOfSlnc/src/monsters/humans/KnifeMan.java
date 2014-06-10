b4e42f5af515b9170ea4231b8273f1509134f14c
wono <wono@live.com>
2014-06-10 12:46:55 -0700

    lowercased directories

---
+++ LgndOfSlnc/src/monsters/humans/KnifeMan.java
@@ -0,0 +1,9 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *        
+ *  JAVA : KNIFEMAN
+ *                                                                  *
+ *      last modified:  2014/06/09                                  *
+ *      first wrote:    2014/06/09                                  *
+ *                                                                  *
+ *      wono (wonho lim: wono@live.com)                             *
+ *                                                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/

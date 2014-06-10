8484f1725abbb85c9fa7bbb60a8c36b9b7610022
wono <wono@live.com>
2014-06-09 18:44:38 -0700

    2014/05.09 commit by wono

---
+++ LgndOfSlnc/src/Monsters/Monster.abst.java
@@ -0,0 +1,10 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *        
+ *  JAVA, ABSTRACT : MONSTERS
+ *                                                                  *
+ *      last modified:  2014/06/09                                  *
+ *      first wrote:    2014/06/09                                  *
+ *                                                                  *
+ *      wono (wonho lim: wono@live.com)                             *
+ *                                                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+

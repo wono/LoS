5f7e7f04be101878e5c0325fa8a487b068189671
wono <wono@live.com>
2014-06-10 12:45:54 -0700

    lowercased directories

--- LgndOfSlnc/src/mons/animals/Animal.abst.java
+++
@@ -1,9 +0,0 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA, ABSTRACT : ANIMAL
- *                                                                  *
- *      last modified:  2014/06/09                                  *
- *      first wrote:    2014/06/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
--- LgndOfSlnc/src/mons/animals/GiantDuck.java
+++
@@ -1,9 +0,0 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA : GIANTDUCK
- *                                                                  *
- *      last modified:  2014/06/09                                  *
- *      first wrote:    2014/06/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/

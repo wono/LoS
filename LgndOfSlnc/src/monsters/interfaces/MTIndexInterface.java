edab799e69d7efbb04f70bc9d2b7117b55ad2861
wono <wono@live.com>
2014-06-12 17:33:41 -0700

    interface name MonsterIndex is replaced to MTIndexInterface. Its index name is also changed. Now all monster index names begin with MTI_ instead of MI_.

---
+++ LgndOfSlnc/src/monsters/interfaces/MTIndexInterface.java
@@ -0,0 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * *        
+ *  JAVA, INTERFACE : MTINDEXINTERFACE
+ *                                                                  *
+ *      last modified:  2014/06/10                                  *
+ *      first wrote:    2014/06/10                                  *
+ *                                                                  *
+ *      wono (wonho lim: wono@live.com)                             *
+ *                                                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+
+package monsters.interfaces;
+
+/**
+ *  All the monster index information of LOS are here.
+ *  
+ *  Note: later on, index numbers need to be re-arranged like this: 
+ *      Animal  type of Monster indices:  0 to 19
+ *      Human   type of Monster indices: 20 to 39
+ */
+public interface MTIndexInterface {
+    
+    // default number of monsters LOS limits currently
+    static final int DEFAULT_MONSTER_MAX_COUNT      = 100; 
+    
+    // total number of sorts of monsters LOS has currently
+    static final int DEFAULT_MONSTER_KINDS_NUMBER   = 4;
+
+    // total number of monster types LOS has currently
+    static final int DEFAULT_MONSTER_TYPES_NUMBER   = 2;
+    
+    // monster indices
+    static final int MTI_ANIMAL_GIANT_DUCK       =  0;
+    static final int MTI_ANIMAL_SQUIRREL_GOLD    =  1;
+    static final int MTI_HUMAN_GUN_MAN           =  2;
+    static final int MTI_HUMAN_KNIFE_MAN         =  3;
+}

bb7b186a0f0c45f742634698589c3dd0618b5c2e
wono <wono@live.com>
2014-06-15 11:24:53 -0700

    monsters/MTName.java and monsters/interfaces/MTNameInterface.java are added to handle all the monster names. Now, each concrete monster instance is represented as monster name within String format.

--- LgndOfSlnc/src/monsters/Monster.java
+++ LgndOfSlnc/src/monsters/Monster.java
@@ -15,7 +15,8 @@ import  static java.lang.System.out;
 public abstract class Monster {
 
     protected String description;
-
+    protected String name;
+    
     /**
      *  preventing to be initialized from outside of this family. 
      *  Use either RandomMonsterGen.GET() to get its concrete instance 
@@ -24,12 +25,19 @@ public abstract class Monster {
      */
     protected Monster ()
     {
-        this.description = MTDescription.GET ( this );
+        this.description    = MTDescription.GET ( this );
+        this.name           = MTName.GET        ( this );
     }
 
     public void describe () 
     {
         out.printf ( "%s\n", this.description );
+    }
+    
+    @Override
+    public String toString ()
+    {
+        return  this.name;
     }
 
 }

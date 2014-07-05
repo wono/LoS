276168013b1a0033bf9eb8b9f2a211e8c7710620
wono <wono@live.com>
2014-07-04 20:20:19 -0700

    A file 'WProcessor.java' is renamed to 'WProcess.java', and minor issues are fixed within its code.

--- src/libraries/WProcess.java
+++ src/libraries/WProcess.java
@@ -8,22 +8,25 @@
  *                                                                  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
 
+package libraries;
+
 import  java.util.function.Predicate;
 import  java.util.function.Consumer;
 
 /**
  *  Wrapper of static method for generics with lambda expression
  */
-public class WProcessor {
+public class WProces {
 
     /**
      *  @param T any type of single object
      *  @param Predicate<T> functional interface
      *  @param Consumer<T> functional interface
      */
-    public static <T> void SINGLE ( T o, 
-                                    Predicate<T> filter, 
-                                    Consumer<T> action      )
+    public static <T> void 
+    WITH_FILTER (   T o, 
+                    Predicate<T> filter, 
+                    Consumer<T> action      )
     {
         if (filter.test(o)) action.accept(o);
     }

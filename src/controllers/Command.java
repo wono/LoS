8ec585f56d3da3bf4e17f41a73376eab4e46d427
wono <wono@live.com>
2014-07-16 17:07:08 -0700

    New file - Command.java

---
+++ src/controllers/Command.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  @author Wonho Lim
+ */
+public enum Command {
+
+    EAST('e'), WEST('w'), SOUTH('s'), NORTH('n'),
+
+    ATTACK('a'), RUN('r');
+    
+    private static Map<Character, Command> COMMANDS;
+    
+    static {
+        COMMANDS = new HashMap<Character, Command>();
+        
+        for (Command c : values()) {
+            COMMANDS.put(c.getShortCommand(), c);
+        }
+    }
+    
+    private char shortCommand;
+    
+    private Command (char c)
+    {
+        shortCommand = c;
+    }
+    
+    publc char getShortCommand ()
+    {
+        return shortCommand;
+    }
+    
+    public static Command GET (char key)
+    {
+        return COMMANDS.get(key);
+    }
+
+}

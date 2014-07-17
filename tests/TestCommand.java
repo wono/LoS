e1ec4aeb10002208c5bd1b8bb4207c241d8f70a1
wono <wono@live.com>
2014-07-16 17:20:35 -0700

    Added missing file

---
+++ tests/TestCommand.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+
+import controllers.Command;
+
+/**
+ *  @author Wonho Lim
+ */
+public class TestCommand {
+
+    public static void main (String[] args)
+    {
+        out.println(Command.GET('a'));
+        out.println(Command.GET('b'));
+        out.println(Command.GET('c'));
+        out.println(Command.GET('d'));
+    }
+}

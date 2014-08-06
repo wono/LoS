2d41d4ef3b97cd7f8de7524e57604077190c97d5
wono <wono@live.com>
2014-08-05 22:37:23 -0700

    Update Dialogue.java

--- src/controllers/interfaces/Dialogue.java
+++ src/controllers/interfaces/Dialogue.java
@@ -1,13 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA, INTERFACE : DIALOGUE
- *                                                                  *
- *      last modified:  2014/07/09                                  *
- *      first wrote:    2014/07/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
-
 package controllers.interfaces;
 
 public interface Dialogue {

4fbc38057fc56159bf515c1e5ddd4d9940034a06
wono <wono@live.com>
2014-08-16 20:34:13 -0700

    Update SquirrelGold.java

--- src/monsters/animals/SquirrelGold.java
+++ src/monsters/animals/SquirrelGold.java
@@ -1,13 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA : SQUIRRELGOLD
- *                                                                  *
- *      last modified:  2014/06/10                                  *
- *      first wrote:    2014/06/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
-
 package monsters.animals;
 
 public class SquirrelGold extends Animal {

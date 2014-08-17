2334a8ba6f3f17d948076da7c17da28c8efb71a3
wono <wono@live.com>
2014-08-16 20:33:38 -0700

    Update GiantDuck.java

--- src/monsters/animals/GiantDuck.java
+++ src/monsters/animals/GiantDuck.java
@@ -1,13 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA : GIANTDUCK
- *                                                                  *
- *      last modified:  2014/06/10                                  *
- *      first wrote:    2014/06/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
-
 package monsters.animals;
 
 public class GiantDuck extends Animal {

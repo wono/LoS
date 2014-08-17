71b96cea35042e85b78ca9455eeef731c1402e97
wono <wono@live.com>
2014-08-16 20:33:53 -0700

    Update Animal.java

--- src/monsters/animals/Animal.java
+++ src/monsters/animals/Animal.java
@@ -1,13 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *        
- *  JAVA, ABSTRACT : ANIMAL
- *                                                                  *
- *      last modified:  2014/06/10                                  *
- *      first wrote:    2014/06/09                                  *
- *                                                                  *
- *      wono (wonho lim: wono@live.com)                             *
- *                                                                  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
-
 package monsters.animals;
 
 import  monsters.Monster;

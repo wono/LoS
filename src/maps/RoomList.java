2435ac3376160f52cf086d1e983555f2419d15ab
wono <wono@live.com>
2014-08-27 17:25:56 -0700

    Update RoomList.java

--- src/maps/RoomList.java
+++ src/maps/RoomList.java
@@ -1,9 +1,9 @@
 package maps;
 
-import  java.util.List;
+import java.util.List;
 
-import  javax.xml.bind.annotation.XmlElement;
-import  javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *  @author Wonho Lim
